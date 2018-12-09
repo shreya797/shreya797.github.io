60dae813ba1bfecb8c15d8f069dba98713c39922
Shreya Chittaranjan <shreya.c.797@gmail.com>
2018-12-09 15:22:23 -0500

    Web
    Update

--- Website/js/script.js
+++ Website/js/script.js
@@ -329,3 +329,23 @@ function update() {
 		requestAnimationFrame(update);
 	}
 	/* ---------------------- MAIN END ----------------------- */
+
+	
+	function animateValue(id, start, end, duration) {
+		var range = end - start;
+		var current = start;
+		var increment = end > start? 1 : +1;
+		var stepTime = Math.abs(Math.floor(duration / range));
+		var obj = document.getElementById(id);
+		var timer = setInterval(function() {
+			current += increment;
+			obj.innerHTML = current;
+			if (current == end) {
+				clearInterval(timer);
+			}
+		}, stepTime);
+	}
+	
+	animateValue("value", 0, 11414, 5000);
+	
+	
